dcc95725710d68acc80944b64bbec3647ac269e7
James24029775 <heartstonejames@gmail.com>
2024-03-04 18:21:47 +0800

    first commit

---
+++ entry.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get install -y openssh-server tmux net-tools ethtool 
+apt-get install -y iputils-ping tcpdump curl build-essential
+apt-get install -y iperf3 iproute2 traceroute dnsutils 
+apt-get install -y git
+
