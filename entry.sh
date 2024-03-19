5d2542927758e5263f40389e7ee3258477c6afa6
James24029775 <74500141+James24029775@users.noreply.github.com>
2024-03-19 20:38:31 +0800

    Update entry.sh
    install vim

--- entry.sh
+++ entry.sh
@@ -3,4 +3,5 @@ apt-get install -y openssh-server tmux net-tools ethtool
 apt-get install -y iputils-ping tcpdump curl build-essential
 apt-get install -y iperf3 iproute2 traceroute dnsutils 
 apt-get install -y git
+apt-get install -y vim
 
